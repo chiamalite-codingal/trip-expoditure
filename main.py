e9e9a28fd04184db0cc72656e9f1e9bef8ea12bb
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-11-08 17:55:34 +0100

    trip expoditure

---
+++ main.py
@@ -0,0 +1,5 @@
+hotelpay = float(input("Enter dollars spent on hotel:"))
+cab = float(input("Enter dollars spent on cab:"))
+flight = float(input("Enter dollars spent on flight:"))
+totalcost = hotelpay + cab + flight
+print("Total is:",totalcost)
